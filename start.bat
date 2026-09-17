e0dd0f69a0c6c742383def98657ae82f16533265
markvoscan-jpg <markvoscan@gmail.com>
2026-09-17 20:19:18 +0500

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "JustAI Server" cmd /k "cd /d server && npm exec -- nodemon --watch src --exec ts-node-esm src/index.ts"
-start "JustAI Web" cmd /k "npm --prefix apps/web exec -- vite --port 4301"
-timeout /t 3 /nobreak > nul
-start "" "http://localhost:4301"
-exit
